60bc7f7c518190a85663d04000db6987369d01cd
badi ifaoui <badi1wow@gmail.com>
2017-02-11 11:49:56 +0100

    romoved blog

--- modules/Blog/Assets/admin/js/routes/index.js
+++
@@ -1,5 +0,0 @@
-import Posts from './posts';
-const BlogRoutes = [
-    ...Posts
-];
-Zexus.routes.push(...BlogRoutes);
--- modules/Blog/helpers.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: badi
- * Date: 9/24/16
- * Time: 8:29 AM
- */
