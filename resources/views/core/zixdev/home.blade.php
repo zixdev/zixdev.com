2436058c40b4e75be3153a2f5aefaffea91d4b4a
badi ifaoui <badi1wow@gmail.com>
2017-02-15 13:25:18 +0100

    added why us section

--- resources/views/core/zixdev/home.blade.php
+++ resources/views/core/zixdev/home.blade.php
@@ -1,10 +1,13 @@
 @extends(site()->partial('core::%s.layouts.default'))
 
 @section('header')
-    @include('core.zixdev.partials._home-header')
+    @include('core.zixdev.home.header')
 @stop
 
 @section('content')
-    @include('core.zixdev.partials._home-about-us')
+    @include('core.zixdev.home.about-us')
+@stop
 
+@section('after-content')
+    @include('core.zixdev.home.why-us')
 @stop
---
+++ resources/views/core/zixdev/home/why-us.blade.php
@@ -0,0 +1,138 @@
+<div class="l-why_us">
+    <div class="container">
+
+
+        <!--Section: Features v.3-->
+        <section class="section feature-box">
+            <div class="text-center">
+                <!--Section heading-->
+                <h1 class="section-heading">Why Choose Us</h1>
+                <!--Section sescription-->
+                <p class="section-description lead white-text">
+                    We Serve All Industries <br>
+                    We stay on top of our industry by being experts in yours.
+                </p>
+            </div>
+
+
+            <!--First row-->
+            <div class="row features-small">
+                <!--Second column-->
+                <div class="col-md-4 center-on-small">
+                    <!--First row-->
+                    <div class="row">
+                        <div class="col-xs-2">
+                            <i class="fa fa-shopping-cart fa-2x"></i>
+                        </div>
+                        <div class="col-xs-10">
+                            <h4 class="feature-title">E-Commerce</h4>
+                            <p>
+                                Raise the bar for your customers with an online experience that exceeds their
+                                expectations and enhances your ROI.
+                            </p>
+                        </div>
+                    </div>
+                    <!--/First row-->
+
+                    <div style="height:50px"></div>
+
+                    <!--Second row-->
+                    <div class="row">
+                        <div class="col-xs-2">
+                            <i class="fa fa-building-o fa-2x"></i>
+                        </div>
+                        <div class="col-xs-10">
+                            <h4 class="feature-title">Enterprise</h4>
+                            <p>
+                                Enhance your business with digital solutions designed with both audience needs
+                                and company objectives in mind.
+                            </p>
+                        </div>
+                    </div>
+                    <!--/Second row-->
+
+                    <div style="height:50px"></div>
+
+                    <!--Third row-->
+                    <div class="row">
+                        <div class="col-xs-2">
+                            <i class="fa fa-lightbulb-o fa-2x"></i>
+                        </div>
+                        <div class="col-xs-10">
+                            <h4 class="feature-title">Startups</h4>
+                            <p>
+                                Build in a foundational strategy to track your company's monetization goals for
+                                web and mobile that continues to convert.
+                            </p>
+                        </div>
+                    </div>
+                    <!--/Third row-->
+                </div>
+                <!--/Second column-->
+
+                <!--First column-->
+                <div class="col-md-4 m-b-r center-on-small-only">
+                    <img src="http://mdbootstrap.com/images/mockups/header-iphone.png" alt="" class="z-depth-0">
+                </div>
+                <!--/First column-->
+
+                <!--Second column-->
+                <div class="col-md-4 center-on-small">
+                    <!--First row-->
+                    <div class="row">
+                        <div class="col-xs-2">
+                            <i class="fa fa-graduation-cap fa-2x"></i>
+                        </div>
+                        <div class="col-xs-10">
+                            <h4 class="feature-title">Education</h4>
+                            <p>
+                                Match your website to your institution’s goals, whether it's awareness,
+                                enrollment, recruitment, or outreach.
+                            </p>
+                        </div>
+                    </div>
+                    <!--/First row-->
+
+                    <div style="height:50px"></div>
+
+                    <!--Second row-->
+                    <div class="row">
+                        <div class="col-xs-2">
+                            <i class="fa fa-code fa-2x"></i>
+                        </div>
+                        <div class="col-xs-10">
+                            <h4 class="feature-title">Technology</h4>
+                            <p>
+                                Put your website to work with cutting edge design, customized content, and
+                                targeted marketing.
+                            </p>
+                        </div>
+                    </div>
+                    <!--/Second row-->
+
+                    <div style="height:50px"></div>
+
+                    <!--Third row-->
+                    <div class="row">
+                        <div class="col-xs-2">
+                            <i class="fa fa-money fa-2x"></i>
+                        </div>
+                        <div class="col-xs-10">
+                            <h4 class="feature-title">Financial</h4>
+                            <p>
+                                We create websites that build trust with customers and credibility with
+                                partners, delivering you measurable results.
+                            </p>
+                        </div>
+                    </div>
+                    <!--/Third row-->
+                </div>
+                <!--/Second column-->
+            </div>
+            <!--/First row-->
+
+        </section>
+        <!--/Section: Features v.3-->
+
+    </div>
+</div>
